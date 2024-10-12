4045303c8f97dbdb83d8b0ce6657fede4c2df00a
root <root@rodionov1586>
2024-10-12 13:46:55 +0400

    add_changes

--- src/log_analyzer.py
+++ src/log_analyzer.py
@@ -110,7 +110,8 @@ def parse_log(file_path: str) -> Generator[LogEntry, None, None]:
         print(f"Error: The file {file_path} was not found.")
     except IOError:
         print(
-            f"Error: An I/O error occurred while handling the file {file_path}."
+            f"Error: An I/O error occurred while"
+            f" handling the file {file_path}."
         )
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
